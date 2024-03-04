f69c1c9527fee5c1908df9f23e0b53b1fbc59cbc
IrfanBain <irfannurbain@gmail.com>
2024-03-04 21:34:09 +0700

    client site rendering

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import styles from '@/styles/products.module.scss'
 
 export default function productPages() {
     const [isLogin,  setIsLogin] = useState(true)
@@ -14,6 +15,7 @@ export default function productPages() {
         }
     }, [])
 
+
     useEffect( () => {
         fetch('/api/product')
         .then(res => res.json())
@@ -21,17 +23,41 @@ export default function productPages() {
             setIsProduct(data.data)
         })
     })
-
     return(
-        <div>
-            <h1>Produtcs Pages</h1>
-            {product.map((item: any) => (
-                <div key={item.id}>
-                    <h3>nama : {item.name}</h3>
-                    <p>harga:  {item.price}</p>
-                    <p style={{marginBottom: '10px'}}>ukuran:  {item.size}</p>
-                </div>
-            ))}
+        <div className={styles.product}>
+            <h1 className={styles.product__title}>Produtcs</h1>
+            <div className={styles.product__content}>
+                {product.length > 0 ? (
+                    <>
+                        {product.map((item: any) => (
+                        <div key={item.id} className={styles.product__content__item}>
+                        <div className={styles.product__content__item__image}>
+                            <img src={item.image} alt={item.name} />
+                        </div>
+                        <h4 className={styles.product__content__item__name}>
+                            {item.name}
+                        </h4>
+                        <p className={styles.product__content__item__category}>
+                            {item.category}
+                        </p>
+                        <p className={styles.product__content__item__price}>
+                            {item.price.toLocaleString('id-ID', {style: 'currency', currency: 'IDR'})}
+                        </p>
+                    </div>
+                ))}
+                    </>
+                ) : (
+                    <>
+                        <div className={styles.product__content__skeleton}>
+                        <div className={styles.product__content__skeleton__image} />
+                        <div className={styles.product__content__skeleton__name} />
+                        <div className={styles.product__content__skeleton__category} />
+                        <div className={styles.product__content__skeleton__price} />
+                        </div>
+                    </>
+                )}
+              
+            </div>
         </div>
     )
 }
